7f40d1666e6304d83eb1da1b3654185f532421e4
Torenia.Founieri <torenia.fournieri@gmail.com>
2023-09-16 18:20:55 +0900

    initial maze

--- brandish.py
+++ brandish.py
@@ -1,21 +1,65 @@
 import random
+from collections import deque
+
+def get_neighboring_walls(pos, width, height):
+    x, y = pos
+    walls = []
+    
+    # Check the four possible directions for walls two units away
+    if x > 2:
+        walls.append((x - 2, y))
+    if x < width - 3:
+        walls.append((x + 2, y))
+    if y > 2:
+        walls.append((x, y - 2))
+    if y < height - 3:
+        walls.append((x, y + 2))
+        
+    return walls
+
+def get_opposite_cell(wall, pos):
+    wx, wy = wall
+    px, py = pos
+    
+    # Determine the direction of the wall relative to the position and return the adjacent cell
+    if wx > px:
+        return wx - 1, wy
+    if wx < px:
+        return wx + 1, wy
+    if wy > py:
+        return wx, wy - 1
+    if wy < py:
+        return wx, wy + 1
+    return None, None  # Return (None, None) if no valid opposite cell is found
 
 def generate_maze(width, height):
     # Create a grid of walls
     maze = [[1 for _ in range(width)] for _ in range(height)]
+    
     # Starting point
     start = (2 * random.randint(0, (width - 1) // 2), 2 * random.randint(0, (height - 1) // 2))
     maze[start[1]][start[0]] = "S"
+    
+    # List of visited cells
+    visited = [start]
+    
+    # Walls adjacent to the starting cell
     walls = get_neighboring_walls(start, width, height)
     
     while walls:
         wall = random.choice(walls)
-        opposite = get_opposite_cell(wall, width, height)
         
-        if maze[wall[1]][wall[0]] == 1 and (maze[opposite[1]][opposite[0]] == 1 or maze[opposite[1]][opposite[0]] == "E"):
-            maze[wall[1]][wall[0]] = 0
-            maze[opposite[1]][opposite[0]] = 0
-            walls += get_neighboring_walls(opposite, width, height)
+        # Get the cell opposite to the current wall relative to the most recently visited cell
+        opposite = get_opposite_cell(wall, visited[-1])
+        
+        # If we have a valid opposite cell, carve the path
+        if opposite != (None, None):
+            ox, oy = opposite
+            if 0 <= ox < width and 0 <= oy < height and maze[oy][ox] == 1:
+                maze[wall[1]][wall[0]] = 0
+                maze[oy][ox] = 0
+                visited.append((ox, oy))
+                walls += get_neighboring_walls((ox, oy), width, height)
         
         walls.remove(wall)
     
@@ -25,36 +69,50 @@ def generate_maze(width, height):
     
     return maze
 
-def get_neighboring_walls(pos, width, height):
-    x, y = pos
-    walls = []
-    if x > 1:
-        walls.append((x - 1, y))
-    if x < width - 2:
-        walls.append((x + 1, y))
-    if y > 1:
-        walls.append((x, y - 1))
-    if y < height - 2:
-        walls.append((x, y + 1))
-    return walls
+def bfs(maze, start, end):
+    """Check if there's a path between start and end using BFS."""
+    visited = set()
+    queue = deque([start])
+    while queue:
+        x, y = queue.popleft()
+        if (x, y) == end:
+            return True  # Path found
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(maze[0]) and 0 <= ny < len(maze) and (nx, ny) not in visited:
+                if maze[ny][nx] == 0 or maze[ny][nx] == "E":
+                    visited.add((nx, ny))
+                    queue.append((nx, ny))
+    return False  # No path found
 
-def get_opposite_cell(wall, width, height):
-    x, y = wall
-    if x > 1 and maze[y][x - 1] != 1:
-        return x - 2, y
-    if x < width - 2 and maze[y][x + 1] != 1:
-        return x + 2, y
-    if y > 1 and maze[y - 1][x] != 1:
-        return x, y - 2
-    if y < height - 2 and maze[y + 1][x] != 1:
-        return x, y + 2
-    return None, None
+def carve_path(maze, start, end):
+    """Carve a direct path between start and end."""
+    x, y = start
+    ex, ey = end
+    while (x, y) != (ex, ey):
+        if x < ex:
+            maze[y][x] = 0
+            x += 1
+        elif x > ex:
+            maze[y][x] = 0
+            x -= 1
+        elif y < ey:
+            maze[y][x] = 0
+            y += 1
+        elif y > ey:
+            maze[y][x] = 0
+            y -= 1
 
+def ensure_path_to_end(maze):
+    start = [(x, y) for y, row in enumerate(maze) for x, cell in enumerate(row) if cell == "S"][0]
+    end = [(x, y) for y, row in enumerate(maze) for x, cell in enumerate(row) if cell == "E"][0]
+    
+    if not bfs(maze, start, end):
+        carve_path(maze, start, end)
 
 # Generate a 15x15 maze for demonstration
-maze_width = 15
-maze_height = 15
-maze = generate_maze(maze_width, maze_height)
+maze = generate_maze(15, 15)
+ensure_path_to_end(maze)
 
 
 
@@ -94,7 +152,7 @@ def display_maze(maze, player_position):
                 display_row.append("P")  # Representing the player with "P"
             else:
                 if cell == 0:
-                    display_row.append(" ")
+                    display_row.append(".")
                 elif cell == 1:
                     display_row.append("#")
                 else:
